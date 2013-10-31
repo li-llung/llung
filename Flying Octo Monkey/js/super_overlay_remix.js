e9e4fa47929e4704b8c5fe99ece81c4f96f741a7
Landon Lung <llung@lynda.com>
2013-10-30 17:34:35 -0700

    Elements rework and last framework items

--- Flying Octo Monkey/js/super_overlay_remix.js
+++ Flying Octo Monkey/js/super_overlay_remix.js
@@ -6,7 +6,9 @@
 	var config = {
 		trigger: '.overlay'
 	};
+	var me;
 	function SuperOverlay (el){
+		me = this;	
 		console.log('yay me');
 		this.show();
 		console.log(el.attr('href'));
@@ -14,14 +16,42 @@
 	}
 	SuperOverlay.prototype.show = function() {
 		console.log('super show');
+		if ($('#modal_bg').length > 0)
+		{
+			$('#modal_bg').remove();
+		}
+		$('<div/>', {
+			id: "modal_bg",
+			'class': "modal_bg spinner",
+			css: {
+				'width': '100%',
+				'height': document.body.clientHeight
+			}
+		}).appendTo("body");
+		$('#modal_bg').fadeTo('fast', '0.50');
+	};
+	SuperOverlay.prototype.close = function() {
+		console.log('super close');
+		$('#overlay').fadeOut();
+		$('#modal_bg').hide();
+	    $("body").trigger("close_overlay");	
 	};
 	$.fn.soverlay_boot = function(){
 		{
+			$(document).off('click').on('click', '.modal_bg', function ()
+			{
+				me.close(); 
+			});
 			$(document).off('click.init').on('click.init', config.trigger, function ()
 			{
 				new SuperOverlay($(this));
 				return false;
 			});		
+			$(document).off('keyup').on('keyup', function (e) {
+	            if (e.keyCode === 27) {
+	               me.close(); 
+	            }
+	        });
 		}
 	};
 	$(document).ready(function ()
