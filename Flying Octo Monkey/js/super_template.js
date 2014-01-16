22d307a8b8fe6783c3d23dc204b7166dbfbe4012
Landon Lung <llung@lynda.com>
2014-01-15 16:55:07 -0800

    adding template for framework

---
+++ Flying Octo Monkey/js/super_template.js
@@ -0,0 +1,58 @@
+/************************************************************
+
+* ========================================================== */
+;(function ( $, window, document, undefined )
+{
+	var pluginName = "zoverlay",
+		trigger = ".overlay",
+		version = "1.0";
+	function SuperOverlay ( element, options ) {
+		var scope = this;
+		this.defaults = {
+            slide_loop: 2
+		};
+		scope.settings = z.cloneData($.extend( {}, scope.defaults, options ));
+		z.updateOptions(element, scope.settings);
+		this.init(element, scope.settings);
+	}
+	SuperOverlay.prototype = {
+		constructor: SuperOverlay,
+        init: function (element, options) {
+        	var scope = this;        
+			if($(document).data(z.cla(options.trigger))) {
+				return true;
+			}
+			$(document).data(z.cla(options.trigger), true);
+        }
+    }
+    var local = SuperOverlay.prototype;
+    // add initialisation
+	z.addInitalisation(pluginName, function() {
+		zem.debug(pluginName + ' initialized');
+		$(trigger).each(function() {
+			var $this = $(this);
+			// this element has already been initialized
+			if($this.data(pluginName)) {
+				return true;
+			}
+			// mark element as initialized
+			$this.data(pluginName, true);
+			new SuperOverlay($this);
+		});
+		$(document).on('click', trigger, function ()
+		{
+			zem.debug('super init show');
+			new SuperOverlay($(this), 'trigger');
+			return false;
+		});
+	});
+	// register UI module
+	z.UIModule({
+		module: pluginName,
+		events: [],
+		init: function() {
+			zem.debug('run '+pluginName+' init');
+			z.initialize(pluginName);
+		}
+	});	
+})( jQuery, window, document );
